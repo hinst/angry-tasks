557ca1a453f3535c4e597ef45153251812fd6cb8
hinst <hinstance@yandex.ru>
2019-06-30 17:14:39 +0200

    build script adjusted

--- src/build.ts
+++ src/build.ts
@@ -10,10 +10,16 @@ async function main() {
     //await buildExec(`dir`);
     try {
         await buildExec(`rmdir /Q /S ${targetFolder}`);
-    } catch (ignored) {}
-    await buildExec(`node_modules\\.bin\\electron-packager . --ignore="src|tsconfig\\.json|\\.vscode"`);
+    } catch (e) {
+        console.log('Could not delete target folder');
+    }
+    const ignored = `--ignore="src|tsconfig\\.json|\\.vscode|${targetFolder}\\.zip|${targetFolder}"`;
+    await buildExec(`node_modules\\.bin\\electron-packager . ${ignored} --app-copyright="Alexander Savinykh, 2019"`);
     await buildExec(`xcopy src\\*.html angry-tasks-win32-x64\\src\\`);
     await buildExec(`xcopy src\\*.css angry-tasks-win32-x64\\resources\\app\\src\\`);
     await buildExec(`xcopy third angry-tasks-win32-x64\\`);
+
+    await buildExec(`del ${targetFolder}.zip`);
+    await buildExec(`"C:\\Program Files\\7-Zip\\7z.exe" a -tzip -bd ${targetFolder} ${targetFolder}`);
 }
 main();
